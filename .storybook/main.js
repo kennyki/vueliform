3cdb8833002c229604faa845d9a4b2557bb75697
kennyki <knyki.12@gmail.com>
2020-09-03 14:43:02 +0800

    add storysource

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-knobs',
-    '@storybook/addon-links'
+    '@storybook/addon-links',
+    '@storybook/addon-storysource'
   ],
 };
