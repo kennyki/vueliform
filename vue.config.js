7381ff9ccdbbd6f62f27c9a673da3b4227a7a305
kennyki <knyki.12@gmail.com>
2020-10-21 16:40:22 +0800

    add bundle analyzer and exclude core-js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: false
+    }
+  },
   configureWebpack: config => {
     config.externals = {
       ...config.externals,
